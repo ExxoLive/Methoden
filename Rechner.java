35895ffc4906412f039fa856651f40c81469b1fd
ExxoLive <exx0@web.de>
2013-05-24 23:51:43 +0300

    Update Rechner.java

--- Rechner.java
+++ Rechner.java
@@ -93,3 +93,53 @@ public class Rechner
 		scanner.close();
 	}	// Ende der main-Methode
 }	// Ende der Rechner-Klasse
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+AUFGABEN : 
+
+I)   Lade dir die Datei Rechner.java in deine Entwicklungsumgebung.
+
+II)  Schaue dir den Quellcode in seiner grundlegenden Funktion an.
+     - Was macht das Programm?
+     - Welche Funktionen hat es?
+    
+III) Führe das Programm aus und teste es mit beliebigen Zahlenwerten.
+
+=====================================================================
+
+1.) Schreibe eine Methode, die
+    a) zwei Zahlen addiert,
+    b) und das Ergebnis zurückgibt.
+    - Nutze die Methode im Programm.
+    
+2.) Schreibe eine Methode, die
+    a) zwei Zahlen subtrahiert
+    b) und das Ergebnis zurückgibt.
+    - Nutze die Methode im Programm.
+    
+3.) Schreibe eine Methode, die
+    a) zwei Zahlen multipliziert
+    b) und das Ergebnis zurückgibt.
+    - Nutze die Methode im Programm.
+    
+4.) Schreibe eine Methode, die
+    a) zwei Zahlen dividiert
+    b) und das Ergebnis zurückgibt.
+    - Nutze die Methode im Programm.
+    
+5.) Schreibe eine Methode, die
+    a) die Optionen des Programms auf der Konsole ausgibt,
+    b) eine Option von der Konsole einließt und
+    c) den Wert zurückgibt.
+    - Nutze die Methode im Programm.
+    
+6.) Schreibe eine Methode, die eine beliebige Funktion erfüllt. (Bspw. Umfang eines Kreises berechnen, oder Quersumme usw.)
+    - Die Methode soll über mindestens einen formalen Parameter verfügen.
+    - Ein Rückgabewert ist nicht unbedingt erforderlich.
+
+Erweiterung des Rechners, um:
+1.) Eine Methode <Rückgabe-Typ> rechne ( <Typ1> zahl1, <Operator-Typ> operator, <Typ2> zahl2 )
+    Benutzung: double ergebnis = rechne( 55, "*", 2 ); // ergebnis = 110
+
+2.) 3 Methoden aus der Klasse java.lang.Math (siehe Java-API Version 7)
